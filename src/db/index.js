325f6a78678d37b3eac9ab0a0bcc9ce3f0d50ee4
gChiroy <gilmain.23@gmail.com>
2024-11-07 20:41:12 -0600

    sistemacom

---
+++ src/db/index.js
@@ -0,0 +1,25 @@
+const { Sequelize } =  require("sequelize");
+const {DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT} = require('../constants/index')
+const { config } = require ('dotenv').config();
+const host = process.env.DB_HOST;
+const sequelize = new Sequelize(
+  DB_NAME, //dbname
+  DB_USERNAME, //username
+  DB_PASSWORD, //password
+
+  
+  {
+    host: "localhost",
+    dialect: "postgres",
+    port: DB_PORT,
+    // dialectOptions: {
+    //   ssl: {
+    //     require: true, // Indica que SSL es obligatorio.
+    //     rejectUnauthorized: false, // Puedes ajustar esto según tus necesidades de seguridad.
+    //   },
+    // },
+   
+  }
+);
+
+module.exports = {sequelize};
