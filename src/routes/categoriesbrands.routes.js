964db5ee629cba039785e9bd274379e48ee048d4
gChiroy <gilmain.23@gmail.com>
2024-11-07 23:10:54 -0600

    sistemacompletos

--- src/routes/categoriesbrands.routes.js
+++ src/routes/categoriesbrands.routes.js
@@ -6,14 +6,20 @@ const router = Router();
 
 
 
-router.get('/allCategories', userAuth, adminAuth, getAllCategories);
-router.get('/category/:id', userAuth, adminAuth, getCateryById);
-router.post('/category', userAuth, adminAuth, createCategory);
-router.put('/category/:id', userAuth, adminAuth, editCategory);
-router.delete('/category/:id', userAuth, adminAuth, deleteCategory);
+router.get('/allCategories', getAllCategories);
+router.get('/category/:id', getCateryById);
+router.post('/category', createCategory);
+router.put('/category/:id', editCategory);
+router.delete('/category/:id', deleteCategory);
 
 
 
 
 module.exports = router;
  
+
+// router.get('/allCategories', userAuth, adminAuth, getAllCategories);
+// router.get('/category/:id', userAuth, adminAuth, getCateryById);
+// router.post('/category', userAuth, adminAuth, createCategory);
+// router.put('/category/:id', userAuth, adminAuth, editCategory);
+// router.delete('/category/:id', userAuth, adminAuth, deleteCategory);
